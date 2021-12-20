033ad9f0e7252bb9a3fda29e629ef99b081c00f7
stevennnccc <92087060+stevennnccc@users.noreply.github.com>
2021-12-21 04:40:51 +0800

    Update hw11_13389_twophasesorting.c

--- hw11_13389_twophasesorting.c
+++ hw11_13389_twophasesorting.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 int compare_char(const void *, const void *);
 int compare_2dchar(const void *, const void *);
 
@@ -13,30 +12,24 @@ int compare_char(const void *a, const void *b)
     const char *cb = (const char *)b;
     return *ca - *cb;
 }
-
 int compare_2dchar(const void *a, const void *b)
 {
     return strcmp(a, b);
 }
-
 int main()
 {
     int N;
     char S[101][7] = {0};
-
     scanf("%d", &N);
-
     for (int i = 0; i < N; i++)
     {
         scanf("%s", S[i]);
         qsort(S[i], 6, sizeof(char), compare_char); // Sorting internal strings
     }
-
     qsort(S, N, 7 * sizeof(char), compare_2dchar); // Sorting strings in different arrays
 
     for (int i = 0; i < N; i++)
     {
-
         if (i == N - 1)
         {
             printf("%s\n", S[i]);
@@ -44,49 +37,4 @@ int main()
         else
             printf("%s ", S[i]);
     }
-    #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int compare_char(const void *, const void *);
-int compare_2dchar(const void *, const void *);
-
-int compare_char(const void *a, const void *b)
-{
-    const char *ca = (const char *)a;
-    const char *cb = (const char *)b;
-    return *ca - *cb;
-}
-
-int compare_2dchar(const void *a, const void *b)
-{
-    return strcmp(a, b);
-}
-
-int main()
-{
-    int N;
-    char S[101][7] = {0};
-
-    scanf("%d", &N);
-
-    for (int i = 0; i < N; i++)
-    {
-        scanf("%s", S[i]);
-        qsort(S[i], 6, sizeof(char), compare_char); // Sorting internal strings
-    }
-
-    qsort(S, N, 7 * sizeof(char), compare_2dchar); // Sorting strings in different arrays
-
-    for (int i = 0; i < N; i++)
-    {
-
-        if (i == N - 1)
-        {
-            printf("%s\n", S[i]);
-        }
-        else
-            printf("%s ", S[i]);
-    }
-    return 0;
 }
